ad6a311af8a6b6afda84fd8cb34379d360c583fc
Megan Souannhaphanh <47784122+s256420@users.noreply.github.com>
2019-04-16 08:23:08 -0400

    Commit

--- CompanyAnimationMeganS/main.lua
+++ CompanyAnimationMeganS/main.lua
@@ -19,6 +19,16 @@ display.setDefault( "background", 1, 1, 1  )
 -- Set the image for the company logo and its variable
 local Platelogo = display.newImage("Images/CompanyLogo.png", 0, 0 ) 
 
+-- Create the visiblility
+Platelogo.alpha = 1
+
+-- Set the scale of the logo
+Platelogo:scale(0.25, 0.25)
+
+-- Set the (x,y) cordinates of the plate logo
+Platelogo.x = display.contentWidth/2
+Platelogo.y = display.contentHeight*1/10
+
 -- Create the local varibles for the sound effects
 local CrashSound = audio.loadSound( "Sounds/CrashSound.mp3")
 
@@ -32,25 +42,19 @@ local function Sound()
 	CrashSoundChannel = audio.play(CrashSound)
 end
 
--- Set the (x,y) cordinates of the plate logo
-Platelogo.x = display.contentWidth/2
-Platelogo.y = display.contentHeight*1/10
-
--- Set the scale of the logo
-Platelogo:scale(0.25, 0.25)
-
 -- Move the plate to the middle of the screen form the top
 transition.moveTo( Platelogo, {x = display.contentWidth/2, y = display.contentHeight/2 , time = 1500})
 
+Platelogo.alpha = Platelogo.alpha - 0.01
+
+
 -- Play the crash sound
 timer.performWithDelay(150, Sound)
-
+--[[
 -- Have the logo fade out 
 transition.fadeOut( Platelogo, { time = 4500 } )
 
--- Create the visiblility
-Platelogo.alpha = 1
-
+--]]
 -----------------------------------------------------------------------------------------
 
 
